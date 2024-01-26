21759dc916df4b87a7e09b379e8329a1e1c76974
Brianca Knight <briancaknight@gmail.com>
2024-01-26 09:24:02 -0800

    add getter for description and pass test 4

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -12,7 +12,7 @@ namespace Bakery.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("bread");
+      Order newOrder = new Order("bread", "delicious!");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -20,7 +20,7 @@ namespace Bakery.Tests
    public void GetTitle_ReturnTitleValue_String()
   {
      string title = "bread";
-     Order newOrder = new Order(title);
+     Order newOrder = new Order(title, "delicious!");
      string result = newOrder.Title;
      Assert.AreEqual(title, result);
   }
@@ -29,12 +29,32 @@ namespace Bakery.Tests
   public void SetTitle_SetOrderTitleValue_String()
   {
     string title = "bread";
-    Order newOrder = new Order(title);
+    Order newOrder = new Order(title, "delicious!");
     string updatedTitle = "pastry";
     newOrder.Title = updatedTitle;
     string result = newOrder.Title;
     Assert.AreEqual(updatedTitle, result);
   }
+
+    [TestMethod]
+   public void GetDescription_ReturnDescriptionValue_String()
+  {
+     string description = "delicious!";
+     Order newOrder = new Order("bread", description);
+     string result = newOrder.Description;
+     Assert.AreEqual(description, result);
+  }
+
+  //    [TestMethod]
+  // public void SetTitle_SetOrderTitleValue_String()
+  // {
+  //   string description = "delicious!";
+  //   Order newOrder = new Order("bread", description);
+  //   string updatedDescription = "foul!";
+  //   newOrder.Description = updatedDescription;
+  //   string result = newOrder.Description;
+  //   Assert.AreEqual(updatedDescription, result);
+  // }
 
 
 
