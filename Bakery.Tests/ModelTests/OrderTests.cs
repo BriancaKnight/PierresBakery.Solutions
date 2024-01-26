42861f266038a51fcc626a0799ed6cf12014394b
Brianca Knight <briancaknight@gmail.com>
2024-01-26 08:58:37 -0800

    add order constructor and pass test 1

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -1,17 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 using Bakery.Models;
+using System.Collections.Generic;
 using System;
 
 namespace Bakery.Tests
 {
-
   [TestClass]
-
+  public class OrderTests
   {
-  
 
-  
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
   }
 }
-
