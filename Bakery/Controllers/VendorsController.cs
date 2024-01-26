2f68dcc3538bb21a0ff7b22c3bff0ad8742ac767
Brianca Knight <briancaknight@gmail.com>
2024-01-26 11:34:36 -0800

    add vendors and vendors id routes to vendor controller

--- Bakery/Controllers/VendorsController.cs
+++ Bakery/Controllers/VendorsController.cs
@@ -24,8 +24,19 @@ namespace Bakery.Controllers
     [HttpPost("/vendors")]
   public ActionResult Create(string vendorName)
   {
-    Category newVendor = new Category(vendorName);
+    Vendor newVendor = new Vendor(vendorName);
     return RedirectToAction("Index");
+  }
+
+    [HttpGet("/vendors/{id}")]
+  public ActionResult Show(int id)
+  {
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    Vendor selectedVendor = Vendor.Find(id);
+    List<Order> vendorOrders = selectedVendor.Orders;
+    model.Add("vendor", selectedVendor);
+    model.Add("orders", vendorOrders);
+    return View(model);
   }
   }
 }
