fcf06cd5453ca2d83ab4aef33b6692d7eea9a50d
Brianca Knight <briancaknight@gmail.com>
2024-01-26 08:51:28 -0800

    add home controller and boilerplate code

---
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using Bakery.Models;
+// using System.Collections.Generic;
+// using System;
+
+// namespace Bakery.Tests
+// {
+//   [TestClass]
+//   public class CategoryTests
+//   {
+
+// [TestMethod]
+// public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+// {
+//   Vendor newVendor = new Vendor();
+//   Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+// }
+
+
+
+
+//   }
+// }
