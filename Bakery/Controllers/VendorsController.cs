4241f32de15abde7ed35522756cc0a7b50c011cd
Brianca Knight <briancaknight@gmail.com>
2024-02-01 18:57:10 -0800

    remove comments in styles.css

--- Bakery/Controllers/VendorsController.cs
+++ Bakery/Controllers/VendorsController.cs
@@ -15,29 +15,29 @@ namespace Bakery.Controllers
       return View(allVendors);
     }
 
-  [HttpGet("/vendors/new")]
-  public ActionResult New()
-  {
-    return View();
-  }
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
     [HttpPost("/vendors")]
-  public ActionResult Create(string vendorName)
-  {
+    public ActionResult Create(string vendorName)
+    {
     Vendor newVendor = new Vendor(vendorName);
     return RedirectToAction("Index");
-  }
+    }
 
     [HttpGet("/vendors/{id}")]
-  public ActionResult Show(int id)
-  {
+    public ActionResult Show(int id)
+    {
     Dictionary<string, object> model = new Dictionary<string, object>();
     Vendor selectedVendor = Vendor.Find(id);
     List<Order> vendorOrders = selectedVendor.Orders;
     model.Add("vendor", selectedVendor);
     model.Add("orders", vendorOrders);
     return View(model);
-  }
+    }
 
     [HttpPost("/vendors/{vendorId}/orders")]
     public ActionResult Create(int vendorId, string title, string description, int price, DateTime date)
